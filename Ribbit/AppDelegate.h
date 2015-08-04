6ba38d36a7e78695c2e4f8a360c05c7ddf7fc401
ag12 <amirg12@hotmail.no>
2015-08-04 16:36:02 +0200

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Amir Ghoreshi on 04/08/15.
+//  Copyright (c) 2015 AM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
